3f74c723a1e1b3919ce0e885cba320626f6c9f33
valeria-debug <valeriaalian79@gmail.com>
2025-09-25 08:49:49 -0500

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -6,4 +6,22 @@ public abstract class Persona {
         this.nombre = nombre;
         this.cedula = cedula;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
     public abstract void mostrarInfo();
+}
